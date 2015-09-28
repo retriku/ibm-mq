fcb73924386fb894f7f9b1f68a271c5b5c40bced
retriku <renars.kudins@gmail.com>
2015-09-28 17:51:38 +0300

    added queue definition

--- provision/shell/script.sh
+++ provision/shell/script.sh
@@ -9,3 +9,8 @@ docker run \
 -e MQ_QMGR_NAME=QM1 \
 -d \
 ibmimages/mqadvanced
+
+sleep 30
+
+docker exec ibmmq bash -c 'setmqaut -m QM1 -t qmgr -p ibmuser +connect +inq +dsp'
+docker exec ibmmq bash -c 'setmqaut -m QM1 -t q -n MESSAGE.QUEUE -p ibmuser +inq +browse +get +put +dsp'
